adfc9b22edf1488e9e59ddda20873e5ccfb1ee8b
ya55sh <yashbahuguna10@gmail.com>
2023-06-23 07:58:20 +0000

    Winner boxes turn red after win.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./styles.css";
 
 let playerState = {
+  winArray: [],
   roundState: 1,
   player1: {
     turn: true,
@@ -29,7 +30,6 @@ for (let i = 0; i < td.length; i++) {
       td[i].innerHTML = playerState.player1.sym;
       playerState.player1.turn = false;
       playerState.player2.turn = true;
-
       console.log("p1 arr", playerState.player1.p1arr);
     } else {
       playerState.player2.p2arr[playerState.roundState - 1] = td[i].innerHTML;
@@ -47,49 +47,63 @@ for (let i = 0; i < td.length; i++) {
       if (playerState.player1.p1arr.includes("00")) {
         if (playerState.player1.p1arr.includes("01")) {
           if (playerState.player1.p1arr.includes("02")) {
-            let x = document.getElementsByClassName("p1");
+            playerState.winArray = ["00", "01", "02"];
             playerState.player1.status = "win";
           }
         }
         if (playerState.player1.p1arr.includes("10")) {
-          if (playerState.player1.p1arr.includes("20"))
+          if (playerState.player1.p1arr.includes("20")) {
+            playerState.winArray = ["00", "10", "20"];
             playerState.player1.status = "win";
+          }
         }
         if (playerState.player1.p1arr.includes("11")) {
-          if (playerState.player1.p1arr.includes("22"))
+          if (playerState.player1.p1arr.includes("22")) {
+            playerState.winArray = ["00", "11", "22"];
             playerState.player1.status = "win";
+          }
         }
       }
 
       if (playerState.player1.p1arr.includes("01")) {
         if (playerState.player1.p1arr.includes("11")) {
-          if (playerState.player1.p1arr.includes("21"))
+          if (playerState.player1.p1arr.includes("21")) {
+            playerState.winArray = ["01", "11", "21"];
             playerState.player1.status = "win";
+          }
         }
       }
 
       if (playerState.player1.p1arr.includes("02")) {
         if (playerState.player1.p1arr.includes("12")) {
-          if (playerState.player1.p1arr.includes("22"))
+          if (playerState.player1.p1arr.includes("22")) {
+            playerState.winArray = ["02", "12", "22"];
             playerState.player1.status = "win";
+          }
         }
         if (playerState.player1.p1arr.includes("11")) {
-          if (playerState.player1.p1arr.includes("20"))
+          if (playerState.player1.p1arr.includes("20")) {
+            playerState.winArray = ["02", "11", "20"];
             playerState.player1.status = "win";
+          }
         }
       }
 
       if (playerState.player1.p1arr.includes("10")) {
         if (playerState.player1.p1arr.includes("11")) {
-          if (playerState.player1.p1arr.includes("12"))
+          if (playerState.player1.p1arr.includes("12")) {
+            playerState.winArray = ["10", "11", "12"];
             playerState.player1.status = "win";
+          }
         }
       }
 
       if (playerState.player1.p1arr.includes("20")) {
         if (playerState.player1.p1arr.includes("21")) {
-          if (playerState.player1.p1arr.includes("22"))
+          if (playerState.player1.p1arr.includes("22")) {
+            playerState.winArray = ["20", "21", "22"];
             playerState.player1.status = "win";
+          }
         }
       }
     }
@@ -98,49 +112,88 @@ for (let i = 0; i < td.length; i++) {
 
     if (playerState.player2.p2arr.includes("00")) {
       if (playerState.player2.p2arr.includes("01")) {
-        if (playerState.player2.p2arr.includes("02"))
+        if (playerState.player2.p2arr.includes("02")) {
+          playerState.winArray = ["00", "01", "02"];
           playerState.player2.status = "win";
+        }
       }
       if (playerState.player2.p2arr.includes("10")) {
-        if (playerState.player2.p2arr.includes("20"))
+        if (playerState.player2.p2arr.includes("20")) {
+          playerState.winArray = ["00", "10", "20"];
           playerState.player2.status = "win";
+        }
       }
       if (playerState.player2.p2arr.includes("11")) {
-        if (playerState.player2.p2arr.includes("22"))
+        if (playerState.player2.p2arr.includes("22")) {
+          playerState.winArray = ["00", "11", "22"];
           playerState.player2.status = "win";
+        }
       }
     }
 
     if (playerState.player2.p2arr.includes("01")) {
       if (playerState.player2.p2arr.includes("11")) {
-        if (playerState.player2.p2arr.includes("21"))
+        if (playerState.player2.p2arr.includes("21")) {
+          playerState.winArray = ["01", "11", "21"];
           playerState.player2.status = "win";
+        }
       }
     }
 
     if (playerState.player2.p2arr.includes("02")) {
       if (playerState.player2.p2arr.includes("12")) {
-        if (playerState.player2.p2arr.includes("22"))
+        if (playerState.player2.p2arr.includes("22")) {
+          playerState.winArray = ["02", "12", "22"];
           playerState.player2.status = "win";
+        }
       }
       if (playerState.player2.p2arr.includes("11")) {
-        if (playerState.player2.p2arr.includes("20"))
+        if (playerState.player2.p2arr.includes("20")) {
+          playerState.winArray = ["02", "11", "20"];
           playerState.player2.status = "win";
+        }
       }
     }
 
     if (playerState.player2.p2arr.includes("10")) {
       if (playerState.player2.p2arr.includes("11")) {
-        if (playerState.player2.p2arr.includes("12"))
+        if (playerState.player2.p2arr.includes("12")) {
+          playerState.winArray = ["10", "11", "12"];
           playerState.player2.status = "win";
+        }
       }
     }
 
     if (playerState.player2.p2arr.includes("20")) {
       if (playerState.player2.p2arr.includes("21")) {
-        if (playerState.player2.p2arr.includes("22"))
+        if (playerState.player2.p2arr.includes("22")) {
+          playerState.winArray = ["20", "21", "22"];
           playerState.player2.status = "win";
+        }
       }
+    }
+
+    if (
+      playerState.player1.status === "win" ||
+      playerState.player2.status === "win"
+    ) {
+      let cName = document.getElementsByClassName("clicked");
+      // console.log(cName[0].className);
+      let totalBlocks = 0;
+      for (let i = 0; i < cName.length; i++) {
+        let temp = cName[i].className.substring(4, 6);
+        if (playerState.winArray.includes(temp)) totalBlocks++;
+      }
+      if (totalBlocks === 3) {
+        playerState.winArray.forEach((element) => {
+          console.log(typeof element);
+          document.getElementsByClassName(
+            "_" + element
+          )[0].style.backgroundColor = "red";
+        });
+      }
+
+      console.log(playerState.winArray);
     }
 
     if (playerState.player2.status === "win") console.log("p2 won");
